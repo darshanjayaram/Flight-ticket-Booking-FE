cbc936b3c6613ac17c20cc21b6c15247e0a7b445
darshanjayaram <darshansj.darshu@gmail.com>
2025-03-04 15:47:23 +0530

    updating

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"; //RTK Query
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8000",
+  baseUrl: "https://flight-ticket-booking-be.onrender.com",
   credentials: "include",
 });
 
